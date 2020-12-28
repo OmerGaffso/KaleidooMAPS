ca5e85884a9533a4be354bdf406cf9e893ffaa93
Flipstacks <Omer231195@gmail.com>
2020-12-28 17:23:18 +0200

    making some progress

---
+++ maps.js
@@ -0,0 +1,18 @@
+var viewer = new Cesium.Viewer("cesiumContainer", {
+    terrainProvider: Cesium.creatWorldterrain()
+});
+var geeMetadata = new GoogleEarthEnterpriseMetadata('http://www.earthenterprise.org/3d');
+var gee = new Cesium.GoogleEarthEnterpriseImageryProvider({
+    metadata: geeMetadata
+});
+var buildingTileSet = viewer.scene.primitives.add(Cesium.createOsmBuildings());
+
+
+// set default view to Israel
+viewer.camera.setView({
+    destination: cesium.Cartesian3.fromDegrees(
+        35.186166,
+        31.771667, 
+        750000.0,
+    )
+});
